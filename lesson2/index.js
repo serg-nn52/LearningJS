4b8ea495c6f0dc838e5b1091097f991f83d54b4a
Sergey Lushkin <serg.nn1985@gmail.com>
2024-03-09 12:15:13 +0300

    добавил файлы второго занятия

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,1 +1,40 @@
-//commit-2
+//строковое преобразование
+const testStringVar = String(undefined);
+
+//преобразование к числу
+//const testNumberVar = Number("      ");
+const testNumberVar = +undefined;
+
+//булевое преобразование
+//const testBooleanVar = Boolean(0);
+const testBooleanVar = !!"0";
+
+//console.log(testBooleanVar, typeof testBooleanVar);
+
+//математические операторы
+// console.log(2 + 5);
+// console.log(2 * 5);
+// console.log(2 - 5);
+// console.log(10 / 2);
+// console.log(11 % 2);
+// console.log(2 ** 3);
+
+//console.log(false + "Sergey");
+
+//операторы сравнения
+//console.log(1 == true);
+
+// console.log(null == undefined); //true
+// console.log(NaN == NaN); //false
+
+//console.log(null === undefined);
+//console.log(Number.isNaN(NaN));
+
+//console.log(5 !== "1");
+
+//сравнения объектов
+const obj1 = {};
+const obj2 = {};
+
+//console.log(obj1 === obj2); //false
+//console.log("2" > "12");
